48413e7e432ef6247437509d4796dbd2917ca2ed
Dmitry Khakhomov <dmi09@hotmail.com>
2018-09-23 03:15:16 +0300

    Finishing task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = [];
+    for (let i = 0; i < str.length; i++) {
+        if (openBracket(str[i]) && closeBracket(str[i]) && brackets[brackets.length - 1] !== str[i]) {
+            brackets.push(str[i])
+        }
+        else if (openBracket(str[i]) && !closeBracket(str[i])) {
+            brackets.push(str[i])
+        }
+        else if (closeBracket(str[i])) {
+            if (checkBracket(str[i], brackets[brackets.length - 1])) {
+                brackets.pop();
+            }
+            else return false
+        }
+    }
+    return  (brackets.length === 0);
+
+    function checkBracket(bracket, openBracket) {
+        for (let array of bracketsConfig) {
+            if (array[0] === openBracket && array[1] === bracket) {
+                return true
+            }
+        }
+        return false
+
+    }
+
+    function openBracket(bracket) {
+        for (let array of bracketsConfig) {
+            if (array[0] === bracket) {
+                return true;
+            }
+        }
+        return false
+    }
+
+    function closeBracket(bracket) {
+        for (let array of bracketsConfig) {
+            if (array[1] === bracket) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
